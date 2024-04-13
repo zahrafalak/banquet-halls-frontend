69fbad49c0f2cad693f3b5435bc0037d0f80d681
Falak Zahra <falak.zahra16@gmail.com>
2024-04-12 21:28:30 -0600

    Create Features component, add icons and feature details

---
+++ src/components/Features/Features.js
@@ -0,0 +1,49 @@
+import cutleryIcon from "../../assets/icons/cutlery.svg";
+import venueIcon from "../../assets/icons/venue.svg";
+import calendarIcon from "../../assets/icons/calendar.svg";
+import Button from "react-bootstrap/Button";
+import "./Features.scss";
+
+const Features = () => {
+  return (
+    <>
+      <h2 className="section-heading">Features</h2>
+      <div className="features">
+        <div className="features__details">
+          <img
+            className="features__icon"
+            src={cutleryIcon}
+            alt="cutlery icon"
+          />
+          <h4>Premium Catering</h4>
+          <p>Exquisite flavors crafted to perfection.</p>
+          <Button className="cta" variant="primary">
+            Explore Menu
+          </Button>{" "}
+        </div>
+        <div className="features__details--middle">
+          <img className="features__icon" src={venueIcon} alt="venue icon" />
+          <h4>Versatile Venues</h4>
+          <p>Ideal settings for any event size.</p>
+          <Button className="cta" variant="primary">
+            View Halls
+          </Button>{" "}
+        </div>
+        <div className="features__details">
+          <img
+            className="features__icon"
+            src={calendarIcon}
+            alt="calendar icon"
+          />
+          <h4>Effortless Booking</h4>
+          <p>Simple, quick, secure reservations.</p>
+          <Button className="cta" variant="primary">
+            Book Now
+          </Button>{" "}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Features;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import MainCarousel from "../../components/Carousel/Carousel";
+import Features from "../../components/Features/Features";
 
 const HomePage = () => {
   return (
     <>
       <Header />
       <MainCarousel />
+      <Features />
       <Footer />
     </>
   );
