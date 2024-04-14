0f48fe6b0cf7da83ee6c3565a55b8cbecc86e29d
Falak Zahra <falak.zahra16@gmail.com>
2024-04-13 18:48:19 -0600

    Create About Us component for the main page

---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,17 @@
+import "./AboutUs.scss";
+const AboutUs = () => {
+  return (
+    <>
+      <h2 className="section-heading">About Us</h2>
+      <p className="about-us">
+        Discover Sky Banquet Hall in Sherwood Park, Alberta—where elegance meets
+        excellence. Our commitment to creating memorable events is matched only
+        by the versatility of our spaces. From grand weddings to professional
+        gatherings, we ensure a seamless experience with a touch of luxury. Step
+        into Sky for an event that soars above the rest.
+      </p>
+    </>
+  );
+};
+
+export default AboutUs;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import MainCarousel from "../../components/Carousel/Carousel";
+import AboutUs from "../../components/AboutUs/AboutUs";
 import Features from "../../components/Features/Features";
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <>
       <Header />
       <MainCarousel />
+      <AboutUs />
       <Features />
       <Footer />
     </>
