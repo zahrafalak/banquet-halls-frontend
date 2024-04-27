d10e12eacd2c6de79d3eef3a06314b2671c3b309
Falak Zahra <falak.zahra16@gmail.com>
2024-04-27 14:07:29 -0600

    Add form validation for first namd last names

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -1,21 +1,54 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import HallsContext from "../../contexts/HallsContext";
 import MenuContext from "../../contexts/MenuContext";
 
 const BookingForm = () => {
+  const [errors, setErrors] = useState({});
+  let currentError = { ...errors };
   const { hallsData } = useContext(HallsContext);
   const { menuPackages } = useContext(MenuContext);
+
+  const validateName = (e) => {
+    const { id, value } = e.target;
+    const nameRegex = /^[a-zA-Z\s]*$/; // Regex to allow only letters and spaces
+
+    if (value.trim() === "") {
+      currentError[id] = "The field cannot be blank";
+    } else if (!nameRegex.test(value)) {
+      currentError[id] = "Please enter a valid name";
+    } else {
+      currentError[id] = "";
+    }
+    setErrors(currentError);
+    console.log(errors);
+  };
 
   return (
     <>
       <Form>
         <Form.Label htmlFor="inputFirstName">First Name</Form.Label>
-        <Form.Control type="text" id="inputFirstName" />
+        <Form.Control
+          type="text"
+          id="inputFirstName"
+          onBlur={validateName}
+          isInvalid={errors.inputFirstName}
+        />
+        {errors.inputFirstName && (
+          <div className="invalid-feedback">{errors.inputFirstName}</div>
+        )}
 
         <Form.Label htmlFor="inputLastName">Last Name</Form.Label>
-        <Form.Control type="text" id="inputLastName" />
+        <Form.Control
+          type="text"
+          id="inputLastName"
+          onBlur={validateName}
+          isInvalid={errors.inputLastName}
+        />
+        {errors.inputLastName && (
+          <div className="invalid-feedback">{errors.inputLastName}</div>
+        )}
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
@@ -35,7 +68,7 @@ const BookingForm = () => {
           {hallsData.map((hall) => {
             return (
               <option key={hall.hall_id} value={hall.name}>
-                {hall.name}
+                {hall.name} - CA$ {hall.price}
               </option>
             );
           })}
@@ -48,7 +81,7 @@ const BookingForm = () => {
           {menuPackages.map((menu) => {
             return (
               <option key={menu.package_id} value={menu.title}>
-                {menu.title}
+                {menu.title} - CA$ {menu.price_per_head} per head
               </option>
             );
           })}
