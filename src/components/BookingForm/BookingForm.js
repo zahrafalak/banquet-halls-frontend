a9d0eb30dc91f9bb4e9ef6bff9051164c602c0a9
Falak Zahra <falak.zahra16@gmail.com>
2024-04-27 16:21:37 -0600

    Add validation for email and event date

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -10,19 +10,46 @@ const BookingForm = () => {
   const { hallsData } = useContext(HallsContext);
   const { menuPackages } = useContext(MenuContext);
 
-  const validateName = (e) => {
+  const validateFormInput = (e) => {
     const { id, value } = e.target;
-    const nameRegex = /^[a-zA-Z\s]*$/; // Regex to allow only letters and spaces
+    const nameRegex = /^[a-zA-Z\s]*$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
     if (value.trim() === "") {
-      currentError[id] = "The field cannot be blank";
-    } else if (!nameRegex.test(value)) {
-      currentError[id] = "Please enter a valid name";
-    } else {
-      currentError[id] = "";
+      // check for empty field
+      currentError[id] = "This field cannot be blank";
+    } else if (id === "inputFirstName" || id === "inputLastName") {
+      // validate name inputs
+      if (!nameRegex.test(value)) {
+        currentError[id] = "Please enter a valid name";
+      } else {
+        currentError[id] = "";
+      }
+    } else if (id === "exampleForm.ControlInput1") {
+      //validate email input
+      if (!emailRegex.test(value)) {
+        currentError[id] = "Invalid date. Please enter a valid email address";
+      } else {
+        currentError[id] = "";
+      }
+    } else if (id === "inputEventDate") {
+      //validate event date input
+      if (!dateRegex.test(value)) {
+        currentError[id] = "Please enter the date in YYYY-MM-DD format";
+      } else {
+        // validate that event date is in future
+        const inputDate = new Date(value);
+        const currentDate = new Date();
+        if (inputDate <= currentDate) {
+          currentError[id] = "Please enter a future date";
+        } else {
+          currentError[id] = "";
+        }
+      }
     }
+
     setErrors(currentError);
-    console.log(errors);
   };
 
   return (
@@ -32,7 +59,7 @@ const BookingForm = () => {
         <Form.Control
           type="text"
           id="inputFirstName"
-          onBlur={validateName}
+          onBlur={validateFormInput}
           isInvalid={errors.inputFirstName}
         />
         {errors.inputFirstName && (
@@ -43,7 +70,7 @@ const BookingForm = () => {
         <Form.Control
           type="text"
           id="inputLastName"
-          onBlur={validateName}
+          onBlur={validateFormInput}
           isInvalid={errors.inputLastName}
         />
         {errors.inputLastName && (
@@ -52,7 +79,17 @@ const BookingForm = () => {
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            onBlur={validateFormInput}
+            isInvalid={errors["exampleForm.ControlInput1"]}
+          />
+          {errors["exampleForm.ControlInput1"] && (
+            <div className="invalid-feedback">
+              {errors["exampleForm.ControlInput1"]}
+            </div>
+          )}
         </Form.Group>
 
         <Form.Label htmlFor="inputEventDate">Date of Event</Form.Label>
@@ -60,11 +97,18 @@ const BookingForm = () => {
           type="text"
           id="inputEventDate"
           placeholder="YYYY-MM-DD"
+          onBlur={validateFormInput}
+          isInvalid={errors.inputEventDate}
         />
+        {errors.inputEventDate && (
+          <div className="invalid-feedback">{errors.inputEventDate}</div>
+        )}
 
         <Form.Label>Choice of Hall</Form.Label>
-        <Form.Select aria-label="Choice of Hall">
-          <option>Choose a hall option</option>
+        <Form.Select aria-label="Choice of Hall" defaultValue="">
+          <option value="" disabled>
+            Choose a hall option
+          </option>
           {hallsData.map((hall) => {
             return (
               <option key={hall.hall_id} value={hall.name}>
@@ -75,8 +119,14 @@ const BookingForm = () => {
         </Form.Select>
 
         <Form.Label>Choice of Menu Package</Form.Label>
-        <Form.Select aria-label="Choice of Menu Package">
-          <option>Choose a menu option</option>
+        <Form.Select
+          aria-label="Choice of Menu Package"
+          defaultValue=""
+          onBlur={validateFormInput}
+        >
+          <option value="" disabled>
+            Choose a menu option
+          </option>
 
           {menuPackages.map((menu) => {
             return (
